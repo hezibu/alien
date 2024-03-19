3d29ada4e9a98246e2880d43e732a3c9e87ab790
hezibu <hezibuba@mail.tau.ac.il>
2024-03-19 13:52:33 +0100

    Updates tests, removed the `sink()` function from the bayesian model output, and created an `snc_model` object to avoid repetitive calculations.

--- tests/testthat/test-modified_belmaker_et_at_2009.R
+++ tests/testthat/test-modified_belmaker_et_at_2009.R
@@ -1,21 +1,23 @@
-example_priors <- c(b0_mu = 0, b1_mu = 0, b0_sd = 0.01, b1_sd = 0.001)
-sink("bayesian-output-sink-file-delete")
-mod_output <- suppressMessages(native_discovery(data = medfish,
-                                                time_col = "time",
-                                                alien_col = "aliens",
-                                                native_col = "natives",
-                                                native_pool_size = 600,
-                                                priors = example_priors,
-                                                chains = 3, parallel_chains = 3)
-)
-closeAllConnections()
-file.remove("bayesian-output-sink-file-delete")
-
 test_that("bayesian_example returns appropriate class object", {
-  expect_true(inherits(mod_output, c("CmdStanMCMC", "CmdStanFit", "R6")))
+  example_priors <- c(b0_mu = 0, b1_mu = 0, b0_sd = 0.01, b1_sd = 0.001)
+  invisible(
+    capture.output(
+      mod_output <-
+        suppressMessages(
+          native_discovery(data = medfish,
+                           time_col = "time",
+                           alien_col = "aliens",
+                           native_col = "natives",
+                           native_pool_size = 600,
+                           priors = example_priors,
+                           chains = 1, parallel_chains = 3)
+        )
+    )
+  )
+  expect_s3_class(mod_output, c("CmdStanMCMC", "CmdStanFit", "R6"))
 })
 
-test_that("native_discovery nee", {
+test_that("native_discovery needs all argument", {
   expect_error(native_discovery(time_col = "time",
                                 alien_col = "aliens",
                                 native_col = "natives",
--- tests/testthat/test-snc.R
+++ tests/testthat/test-snc.R
@@ -1,35 +1,35 @@
 test_that("snc function returns error when number of init values does not match number of parameters to estimate", {
-  expect_error(snc(y = sfestuary, mu = ~ time, y = ~ time, init = rnorm(3), growth = T, data = data.frame(y = sfestuary, time = seq_along(sfestuary))))
-  expect_error(snc(y = sfestuary, mu = ~ time, y = ~ time, init = rnorm(4), growth = T, data = data.frame(y = sfestuary, time = seq_along(sfestuary))))
-  expect_error(snc(y = sfestuary, mu = ~ time, y = ~ time, init = rnorm(5), growth = F, data = data.frame(y = sfestuary, time = seq_along(sfestuary))))
+  expect_error(snc(y = sfestuary, mu = ~ time, pi = ~ time, init = rnorm(3), growth = T, data = data.frame(y = sfestuary, time = seq_along(sfestuary))))
+  expect_error(snc(y = sfestuary, mu = ~ time, pi = ~ time, init = rnorm(4), growth = T, data = data.frame(y = sfestuary, time = seq_along(sfestuary))))
+  expect_error(snc(y = sfestuary, mu = ~ time, pi = ~ time, init = rnorm(5), growth = F, data = data.frame(y = sfestuary, time = seq_along(sfestuary))))
   expect_no_error(snc(y = sfestuary, mu = ~ time, pi = ~ time, init = rep(0,5), growth = T, data = data.frame(y = sfestuary, time = seq_along(sfestuary))))
   expect_no_error(snc(y = sfestuary, mu = ~ time, pi = ~ time, growth = T, data = data.frame(y = sfestuary, time = seq_along(sfestuary))))
 })
 
 test_that("snc function warns when no forumlas are given", {
-  expect_message(snc(y = sfestuary, data = data.frame(y = sfestuary, time = seq_along(sfestuary))))
-  expect_message(snc(y = sfestuary, mu = ~ time,  data = data.frame(y = sfestuary, time = seq_along(sfestuary))))
-  expect_message(snc(y = sfestuary, pi = ~ time, data = data.frame(y = sfestuary, time = seq_along(sfestuary))))
+  expect_message(snc(y = sfestuary, data = data.frame(y = sfestuary, time = seq_along(sfestuary)))) |> suppressMessages()
+  expect_message(snc(y = sfestuary, mu = ~ time,  data = data.frame(y = sfestuary, time = seq_along(sfestuary)))) |> suppressMessages()
+  expect_message(snc(y = sfestuary, pi = ~ time, data = data.frame(y = sfestuary, time = seq_along(sfestuary)))) |> suppressMessages()
 })
 
 test_that("snc function stops when no data is given", {
-  expect_error(snc(y = sfestuary, mu = ~ time, pi  = ~time))
-  expect_no_error(snc(y = sfestuary))
+  expect_error(snc(y = sfestuary, mu = ~ time, pi  = ~ time))
   expect_no_error(snc(y = sfestuary, mu = ~ time, pi  = ~time, data = data.frame(time = seq_along(sfestuary))))
 })
 
+model_ouput <- snc(y = sfestuary, mu = ~ time, pi  = ~time, data = data.frame(time = seq_along(sfestuary)))
 
 test_that("plot_snc return a ggplot object", {
-  expect_equal(class(plot_snc(snc(sfestuary), cumulative = F)), c("gg","ggplot"))
-  expect_equal(class(plot_snc(snc(sfestuary), cumulative = T)), c("gg","ggplot"))
+  expect_s3_class(plot_snc(model_ouput, cumulative = F), c("gg","ggplot"))
+  expect_s3_class(plot_snc(model_ouput, cumulative = T), c("gg","ggplot"))
 })
 
 test_that("plot_snc does not work for other object", {
   expect_error(plot_snc(sfestuary, cumulative = F))
-  expect_no_error(plot_snc(snc(sfestuary), cumulative = F))
+  expect_no_error(plot_snc(model_ouput, cumulative = F))
 })
 
 test_that("summary_snc does not work for other object", {
   expect_error(summary_snc(sfestuary))
-  expect_no_error(summary_snc(snc(sfestuary)))
+  expect_no_error(summary_snc(model_ouput))
 })
